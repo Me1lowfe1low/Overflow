9550b1fb0cdc8a106520b808cdd6f75e233ba275
C'est une pomme <dmgordienko@gmail.com>
2022-12-17 18:20:09 +0400

    Initial Commit

---
+++ Overflow/OverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverflowApp.swift
+//  Overflow
+//
+//  Created by Дмитрий Гордиенко on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct OverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
