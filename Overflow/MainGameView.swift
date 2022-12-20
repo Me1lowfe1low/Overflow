8dc424b0d85a9dc1680ef7d1c5c7cad4106f43d2
C'est une pomme <dmgordienko@gmail.com>
2022-12-20 18:16:13 +0400

    Edited the model of vessel view 'VesselModel.swift' and added its content - 'WaterFlowModel.swift' Now the level of water in the vessel could be calculated and instantly showed

--- Overflow/MainGameView.swift
+++ Overflow/MainGameView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct MainGameView: View {
+    @State var currentWaterLevel: CGFloat = 0.99
+    @State var vesselSize: CGFloat = 0.5
+    
     var body: some View {
             VStack {
                 ZStack{
                     RoundedRectangle(cornerRadius: 4.0)
                         .stroke(lineWidth: 2.0)
                         .aspectRatio(3/4, contentMode: .fill)
-                    Vessel()
+                    Vessel(vesselHeighRatio: vesselSize)
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.white,Color.gray]), startPoint: .leading, endPoint: .trailing))
+                    Vessel(vesselHeighRatio: vesselSize)
+                        .stroke(.black,style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+                   WaterFlow(fillProgress: currentWaterLevel, vesselSizeRatio: vesselSize)
+                        .fill(Color.blue).opacity(0.6)
+                        .mask {
+                            Vessel(vesselHeighRatio: vesselSize)
+                        }
                 }
                 Spacer()
+                Divider()
                 HStack {
                     // Where should be the area with dragable objects
                     // Once we'll choose the object and drop it to the area above - there would be calculation of object
--- Overflow/VesselView.swift
+++ Overflow/VesselView.swift
@@ -8,25 +8,33 @@
 import SwiftUI
 
 struct VesselView: View {
+    @State var heightRatio: CGFloat = 0.2
+    
+    
     var body: some View {
-        Vessel()
+        Vessel(vesselHeighRatio: heightRatio)
     }
 }
 
 struct Vessel: Shape {
+    var vesselHeighRatio: CGFloat
+    
     func path(in rect: CGRect) -> Path {
         Path { path in
-            path.move(to: CGPoint(x: rect.midX-rect.midX/4, y: rect.midY-rect.midY/30))
-            path.addLine(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY-rect.midY/30))
-            path.addLine(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY))
-            path.addLine(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY))
-            path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY+rect.midY/8), control: CGPoint(x: rect.midX,y: rect.midY))
-            //path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/6,y: rect.midY+rect.midY/2), control: CGPoint(x: rect.midX+rect.midX/2,y: rect.midY+rect.midY/4))
-            path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/6,y: rect.maxY-rect.maxY/30), control: CGPoint(x: rect.midX+rect.midX/2,y: rect.midY+rect.midY/4))
-            //path.addLine(to: CGPoint(x: rect.midX-rect.midX/6,y:  rect.midY+rect.midY/2))
-            path.addLine(to: CGPoint(x: rect.midX-rect.midX/6,y:  rect.maxY-rect.maxY/30))
-            path.addQuadCurve(to: CGPoint(x: rect.midX-rect.midX/4,y: rect.midY+rect.midY/8), control: CGPoint(x: rect.midX-rect.midX/2,y: rect.midY+rect.midY/4))
-            path.addQuadCurve(to: CGPoint(x: rect.midX-rect.midX/4,y: rect.midY), control: CGPoint(x: rect.midX,y: rect.midY))
+            let vesselHeight = ( 1 - vesselHeighRatio ) * rect.height
+            let vesselMidHeigh = vesselHeight / 2
+            let vesselBottleNeck = vesselMidHeigh-vesselMidHeigh / 8
+            let vesselHeigh = 5 * vesselHeight / 12
+            
+            path.move(to: CGPoint(x: rect.midX-rect.midX/4, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX+rect.midX/4, y: rect.maxY))
+            path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/4, y: vesselMidHeigh), control: CGPoint(x: rect.maxX-rect.midX/6, y: vesselMidHeigh))
+            path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/4, y: vesselBottleNeck), control: CGPoint(x: rect.midX,y: vesselMidHeigh))
+            path.addLine(to: CGPoint(x: rect.midX+rect.midX/4, y: vesselHeigh))
+            path.addLine(to: CGPoint(x: rect.midX-rect.midX/4, y: vesselHeigh))
+            path.addLine(to: CGPoint(x: rect.midX-rect.midX/4, y: vesselBottleNeck))
+            path.addQuadCurve(to: CGPoint(x: rect.midX-rect.midX/4, y: vesselMidHeigh), control: CGPoint(x: rect.midX, y: vesselMidHeigh))
+            path.addQuadCurve(to: CGPoint(x: rect.midX-rect.midX/4, y: rect.maxY), control: CGPoint(x: rect.midX/6, y: vesselMidHeigh))
         }
     }
 }
@@ -36,3 +44,18 @@ struct VesselView_Previews: PreviewProvider {
         VesselView()
     }
 }
+
+
+
+
+
+/*path.move(to: CGPoint(x: rect.midX-rect.midX/4, y: rect.midY-rect.midY/vesselSize))
+path.addLine(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY-rect.midY/vesselSize))
+path.addLine(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY))
+path.addLine(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY))
+path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/4,y: rect.midY+rect.midY/8), control: CGPoint(x: rect.midX,y: rect.midY))
+path.addQuadCurve(to: CGPoint(x: rect.midX+rect.midX/6,y: rect.maxY-rect.maxY/vesselSize), control: CGPoint(x: rect.midX+rect.midX/2,y: rect.midY+rect.midY/4))
+path.addLine(to: CGPoint(x: rect.midX-rect.midX/6,y:  rect.maxY-rect.maxY/vesselSize))
+path.addQuadCurve(to: CGPoint(x: rect.midX-rect.midX/4,y: rect.midY+rect.midY/8), control: CGPoint(x: rect.midX-rect.midX/2,y: rect.midY+rect.midY/4))
+path.addQuadCurve(to: CGPoint(x: rect.midX-rect.midX/4,y: rect.midY), control: CGPoint(x: rect.midX,y: rect.midY))
+path.addLine(to: CGPoint(x: rect.midX-rect.midX/4, y: rect.midY-rect.midY/vesselSize))*/
